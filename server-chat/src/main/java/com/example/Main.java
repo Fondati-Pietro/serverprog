2f5a90e37c4d91ee550ea507820e59175b10d2c9
lai2-24 <lai2-24@itismeucci.com>
2024-11-05 14:39:19 +0100

    secondo commit

--- server-chat/src/main/java/com/example/Main.java
+++ server-chat/src/main/java/com/example/Main.java
@@ -1,7 +1,22 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ArrayList<MyThread> listaUtenti = new ArrayList();
         System.out.println("Hello world!");
+         ServerSocket server = new ServerSocket(3000);
+        System.out.println("Server avviato!");
+        do{
+            Socket s = server.accept();
+            System.out.println("Un client si è collegato!");
+            MyThread t = new MyThread(s, listaUtenti);
+            listaUtenti.add(t);
+            t.start();
+        }while(true);
     }
 }
